987ea35084f2e086661d3373460172ab71144d05
Ismael <ismael@lonuncavisto.com>
2023-04-25 22:18:08 +0200

    Creación esqueleto formulario

--- index.php
+++ index.php
@@ -4,11 +4,33 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Inicio de sesión</title>
 </head>
 <body>
 
-hola
+
+<form action="">
+    <ul>
+        <li>
+            <label for="email">Email:</label>
+            <input type="email" placeholder="ejemplo@prueba.com" name="email" value="">
+        </li>
+        <li>
+            <label for="password">Pasword</label>
+            <input type="password" name="password">
+        </li>
+        <li>
+            <button type="submit">Login</button>
+        </li>
+        <li>
+            <a href="">¿Olvidó su contraseña?</a>
+        </li>
+        <li>
+            <a href="">Crear una nueva cuenta</a>
+        </li>
+    </ul>
+</form>
+
     
 </body>
 </html>
